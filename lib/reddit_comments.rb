a739f2be3ca07faa132bcf576d955bb80018bd59
Esther Leytush <eleytush@gmail.com>
2016-06-21 18:12:13 -0400

    add tests and refactor

--- lib/reddit_comments.rb
+++ lib/reddit_comments.rb
@@ -6,63 +6,73 @@ require 'json'
 module RedditComments
   class IncorrectLinkFormat < StandardError; end
 
-  def self.recursive_comment_digging(child, comments=[])
-    post = {}
-    post["id"] = child["id"]
-    post["parent_id"] = child["parent_id"]
-    post["author"] = child["author"]
-    post["body"] = child["body"]
+  class GetComments
+    attr_accessor :url, :comments, :request
 
-    comments << post
+    def initialize(url)
+      @url = url
+      @comments = []
+    end
 
-    if child["replies"] != nil && child["replies"] != ""
-      child["replies"]["data"]["children"].each do |comment|
-        comments << self.recursive_comment_digging(comment["data"])
+    def recursive_comment_digging(child, comments=[])
+      post = {}
+      post["id"] = child["id"]
+      post["parent_id"] = child["parent_id"]
+      post["author"] = child["author"]
+      post["body"] = child["body"]
+
+      comments << post
+
+      if child["replies"] != nil && child["replies"] != ""
+        child["replies"]["data"]["children"].each do |comment|
+          comments << self.recursive_comment_digging(comment["data"])
+        end
+      end
+      comments
+    end
+
+    def get_comments
+      @post[1]["data"]["children"].each do |child|
+        @comments << recursive_comment_digging(child["data"])
+      end
+      @comments = comments.flatten
+    end
+
+    def parse_post
+      uri = URI.parse(url.dup)
+      http = Net::HTTP.new(uri.host)
+      request = Net::HTTP::Get.new(uri.request_uri)
+      request.initialize_http_header({"User-Agent" => "Reddit_Comments_Gem_0.1_mindplace"})
+      body = http.request(request)
+      @post = JSON.parse(body.body)
+    end
+
+    def append_json
+      unless url.match(/\/.json\z/)
+        if url[-1] == "/"
+          @url = url + ".json"
+        else
+          @url = url + "/.json"
+        end
       end
     end
-    comments
-  end
 
-  def self.parse_post(post)
-    uri = URI.parse(post.dup)
-    http = Net::HTTP.new(uri.host)
-    request = Net::HTTP::Get.new(uri.request_uri)
-    request.initialize_http_header({"User-Agent" => "Reddit_Comments_Gem_0.1_mindplace"})
-    body = http.request(request)
-    JSON.parse(body.body)
-  end
-
-  def self.get_comments(post)
-    comments = []
-    post[1]["data"]["children"].each do |child|
-      comments << recursive_comment_digging(child["data"])
-    end
-    comments.flatten
-  end
-
-  def self.append_json(link)
-    if link.match(/\/.json\z/)
-      return link
+    def link_tested
+      unless url.match(/https:\/\/www.reddit.com\/r\/(.*)\/comments\/(.*)\/(.*)\//)
+        raise IncorrectLinkFormat, "Not an acceptable Reddit link, please see documentation."
+      end
     end
 
-    if link[-1] == "/"
-      link = link + ".json"
-    else
-      link = link + "/.json"
-    end
-    link
-  end
-
-  def self.link_tested(link)
-    unless link.match(/https:\/\/www.reddit.com\/r\/(.*)\/comments\/(.*)\/(.*)\//)
-      raise IncorrectLinkFormat, "Not an acceptable Reddit link, please see documentation."
+    def retrieve
+      link_tested
+      append_json
+      parse_post
+      get_comments
     end
   end
 
   def self.retrieve(link)
-    self.link_tested(link)
-    link = self.append_json(link)
-    post = self.parse_post(link)
-    self.get_comments(post)
+    comments = GetComments.new(link)
+    comments.retrieve
   end
 end
--- spec/reddit_comments_spec.rb
+++ spec/reddit_comments_spec.rb
@@ -1,43 +1,66 @@
 require 'spec_helper'
+require 'pry'
+require 'json'
 
 describe RedditComments do
   it 'has a version number' do
     expect(RedditComments::VERSION).not_to be nil
   end
 
-  describe "self.link_tested" do
+  describe 'self.link_tested' do
     it 'raises error if given a non-Reddit link' do
-      url = "http://www.example.com"
+      url = 'http://www.example.com'
       expect{ RedditComments.link_tested(url) }.to raise_error(RedditComments::IncorrectLinkFormat)
     end
 
-    it "raises error for Reddit link if not correct format" do
-      url = "https://www.reddit.com/r/worldnews/comments/were_living_worse_than_in_a_war_venezuelas/"
+    it 'raises error for Reddit link if not correct format' do
+      url = 'https://www.reddit.com/r/worldnews/comments/were_living_worse_than_in_a_war_venezuelas/'
       expect{ RedditComments.link_tested(url) }.to raise_error(RedditComments::IncorrectLinkFormat)
     end
 
     it 'passes an acceptabe Reddit link' do
-      url = "https://www.reddit.com/r/worldnews/comments/4p4orb/were_living_worse_than_in_a_war_venezuelas/"
+      url = 'https://www.reddit.com/r/worldnews/comments/4p4orb/were_living_worse_than_in_a_war_venezuelas/'
       expect{ RedditComments.link_tested(url) }.not_to raise_error
     end
   end
 
-  describe "self.append_json" do
+  describe 'self.append_json' do
     it 'appends .json to the link if not present' do
-      url = "https://www.reddit.com/r/worldnews/comments/4p4orb/were_living_worse_than_in_a_war_venezuelas/"
-      updated = url + ".json"
+      url = 'https://www.reddit.com/r/worldnews/comments/4p4orb/were_living_worse_than_in_a_war_venezuelas/'
+      updated = url + '.json'
       expect(RedditComments.append_json(url)).to eq(updated)
     end
 
     it 'does not append if present' do
-      url = "https://www.reddit.com/r/worldnews/comments/4p4orb/were_living_worse_than_in_a_war_venezuelas/.json"
+      url = 'https://www.reddit.com/r/worldnews/comments/4p4orb/were_living_worse_than_in_a_war_venezuelas/.json'
       expect(RedditComments.append_json(url)).to eq(url)
     end
   end
 
-  describe "self.parse_post" do
+  describe 'self.parse_post' do
+    it 'takes a url and returns JSON object of url content' do
+      url = 'https://www.reddit.com/r/worldnews/comments/4p4orb/were_living_worse_than_in_a_war_venezuelas/.json'
+      result = RedditComments.parse_post(url)
+      expect(result).to be_an_instance_of(Array)
+      expect(result[0]).to be_an_instance_of(Hash)
+    end
   end
 
-  describe "self.get_comments" do
+  describe 'self.get_comments' do
+    let(:post_body) { RedditComments.parse_post("https://www.reddit.com/r/worldnews/comments/4p71mu/migrant_protests_calais_france_tried_to_force_way/.json")}
+    let(:comments) { RedditComments.get_comments(post_body) }
+
+    it 'returns an array of comment hashes' do
+      expect(comments).to be_an_instance_of(Array)
+
+      comments.each do |comment|
+        expect(comment).to be_an_instance_of(Hash)
+      end
+    end
+
+    it 'includes expected comment text' do
+      comment = comments.select{|c| c["body"] == "That moment you realize brexit does nothing to stop this."}
+      expect(comment).not_to be_empty
+    end
   end
 end
